f3ea8606c2b598c20e85d8e3669269daed19cd40
Menna <mennav@protonmail.com>
2024-01-29 18:49:17 -0300

    update output

--- gcp_enum.py
+++ gcp_enum.py
@@ -16,53 +16,66 @@ def authenticate_service_account(json_file_path, key_file_path):
     except Exception as e:
         print(f"Failed to authenticate: {str(e)}")
 
-def list_resources(resource_type, command):
+def test_resource_access(resource_type, command, output_file):
     try:
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE)
-        resources = result.stdout.decode().split('\n')
-        for resource in resources:
-            print(f"{resource_type}: {resource}")
+        result = subprocess.run(command, shell=True, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            access_status = f"{resource_type} access granted"
+        else:
+            access_status = f"{resource_type} access denied"
+
+        # Print the access status directly and save the full output to the specified file
+        print(access_status)
+        with open(output_file, "a") as file:
+            file.write(access_status + "\n")
+            file.write(result.stdout)
+            file.write("\n")
+            file.write(result.stderr)
+            file.write("\n")
 
     except Exception as e:
-        print(f"Failed to list {resource_type}: {str(e)}")
+        print(f"Failed to test {resource_type} access: {str(e)}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Authenticate and list GCP resources")
+    parser = argparse.ArgumentParser(description="Authenticate and test GCP resource access")
     parser.add_argument("-f", "--file", required=True, help="Path to the JSON key file")
+    parser.add_argument("-o", "--output", required=True, help="Path to the output file")
     args = parser.parse_args()
+
+    # Clear the content of the output file
+    with open(args.output, "w") as file:
+        file.write("")
 
     # Authenticate using the provided JSON key file
     authenticate_service_account(args.file, args.file)
 
-    # List GCP resources
+    # Test GCP resource access and save the results
     resource_commands = {
         "Cloud Storage Buckets": "gsutil ls",
-        "Cloud Storage Buckets2": "gcloud storage buckets list", 
         "Pub/Sub topics": "gcloud pubsub topics list",
         "BigQuery": "bq ls",
         "Compute Engine Firewall Rules": "gcloud compute firewall-rules list",
-        "Compute Engine Network Subnets": "gcloud compute networks subnets list",
-        "IAM Service Accounts": "gcloud iam service-accounts list",
-        "GCP Projects": "gcloud projects list",
-        "Compute Engine Instance Templates": "gcloud compute instance-templates list",
-        "Compute Engine Images": "gcloud compute images list",
-        "Cloud Functions": "gcloud functions list",
-        "Pub/Sub Subscriptions": "gcloud pubsub subscriptions list",
-        "Compute Engine Backend Services": "gcloud compute backend-services list",
-        "AI Platform Models": "gcloud ai-platform models list",
-        "AI Platform Jobs": "gcloud ai-platform jobs list",
-        "Cloud Run Managed Services": "gcloud run services list --platform=managed",
-        "Cloud Run GKE Services": "gcloud run services list --platform=gke",
-        "Cloud SQL Instances": "gcloud sql instances list",
-        "Cloud Spanner Instances": "gcloud spanner instances list",
-        "Cloud Bigtable Instances": "gcloud bigtable instances list",
-        "Cloud Filestore Instances": "gcloud filestore instances list",
-        "Kubernetes Engine Clusters": "gcloud container clusters list",
-        "Container Images": "gcloud container images list",
-        "Secrets": "gcloud secrets list",
-        "Cloud KMS Keyrings": "gcloud kms keyrings list --location=global",
+    	"Compute Engine Network Subnets": "gcloud compute networks subnets list",
+    	"IAM Service Accounts": "gcloud iam service-accounts list",
+    	"GCP Projects": "gcloud projects list",
+    	"Compute Engine Instance Templates": "gcloud compute instance-templates list",
+    	"Compute Engine Images": "gcloud compute images list",
+    	"Cloud Functions": "gcloud functions list",
+    	"Pub/Sub Subscriptions": "gcloud pubsub subscriptions list",
+    	"Compute Engine Backend Services": "gcloud compute backend-services list",
+    	"AI Platform Models": "gcloud ai-platform models list",
+    	"AI Platform Jobs": "gcloud ai-platform jobs list",
+    	"Cloud Run Managed Services": "gcloud run services list --platform=managed",
+    	"Cloud Run GKE Services": "gcloud run services list --platform=gke",
+    	"Cloud SQL Instances": "gcloud sql instances list",
+    	"Cloud Spanner Instances": "gcloud spanner instances list",
+    	"Cloud Bigtable Instances": "gcloud bigtable instances list",
+    	"Cloud Filestore Instances": "gcloud filestore instances list",
+    	"Kubernetes Engine Clusters": "gcloud container clusters list",
+    	"Container Images": "gcloud container images list",
+    	"Secrets": "gcloud secrets list",
+    	"Cloud KMS Keyrings": "gcloud kms keyrings list",
     }
 
     for resource_type, command in resource_commands.items():
-        print(f"Listing {resource_type}:")
-        list_resources(resource_type, command)
+        test_resource_access(resource_type, command, args.output)
